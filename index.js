5049eee61a20c65e0d046b2735c7021733d3112a
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-02-09 15:52:18 +0530

    hello

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.querySelectorAll(".controls").forEach((control) => {
     selectedLocation = JSON.parse(savedLocation);
   }
 
+  
   locationPin.addEventListener("click", () => {
     if (selectedLocation || isFetching) return; // Skip if location is already fetched or fetching
 
